0347e7ab4d6d9a077e41e7e5c49ae4d4064b0a2a
Xandr03 <alexander.bakas@gmail.com>
2024-04-08 12:41:19 +0200

    Changes

--- Source/SPM/SPMCharacter.cpp
+++ Source/SPM/SPMCharacter.cpp
@@ -32,13 +32,13 @@ ASPMCharacter::ASPMCharacter()
 
 	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
 	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	FiringPoint = CreateDefaultSubobject<USceneComponent>(TEXT("FiringPoint"));
 	Mesh1P->SetOnlyOwnerSee(true);
 	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
 	Mesh1P->bCastDynamicShadow = false;
 	Mesh1P->CastShadow = false;
 	//Mesh1P->SetRelativeRotation(FRotator(0.9f, -19.19f, 5.2f));
 	Mesh1P->SetRelativeLocation(FVector(-30.f, 0.f, -150.f));
-
 }
 
 void ASPMCharacter::BeginPlay()
