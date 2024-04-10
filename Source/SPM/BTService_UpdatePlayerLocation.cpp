100a8ccf8a113ac61b0564bbb6835dc74d223a59
PhilipCarlsson5 <phillec01@gmail.com>
2024-04-10 09:52:21 +0200

    Spiderbot Chase Player

---
+++ Source/SPM/BTService_UpdatePlayerLocation.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTService_UpdatePlayerLocation.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+UBTService_UpdatePlayerLocation::UBTService_UpdatePlayerLocation()
+{
+	NodeName = TEXT("Update Player Location");
+}
+
+void UBTService_UpdatePlayerLocation::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	if (OwnerComp.GetAIOwner() == nullptr)
+	{
+		return;
+	}
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (PlayerPawn == nullptr)
+	{
+		return;
+	}
+
+	OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), PlayerPawn->GetActorLocation());
+}
+
