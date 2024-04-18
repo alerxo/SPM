591ec26a66a128419f742d280923dfb0856ade1a
Xandr03 <alexander.bakas@gmail.com>
2024-04-18 15:06:00 +0200

    HazardousWater

---
+++ Source/SPM/HazardousWater.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+
+#include "DamageComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "HazardousWater.h"
+
+
+// Sets default values
+AHazardousWater::AHazardousWater()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	//Setup Water plane Static mesh component
+	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Plane"));
+	RootComponent = Water;
+	
+	//Setup DamageComponents
+	DamageComponent = CreateDefaultSubobject<UDamageComponent>(TEXT("Damage"));
+	
+	//Setup Water collision
+	WaterCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("WaterCollision"));
+	WaterCollision->SetupAttachment(RootComponent);
+
+
+	Timer = TimeUntilDamage;
+
+
+
+
+	
+ }
+
+// Called when the game starts or when spawned
+void AHazardousWater::BeginPlay()
+{
+	Super::BeginPlay();
+	Player = UGameplayStatics::GetPlayerCharacter(GetWorld(),0);
+}
+
+// Called every frame
+void AHazardousWater::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	if(bIsPlayerOnWater)
+	{
+		if(Timer <= 0)
+		{
+			DamageActor();
+		}
+		Timer -= 1 * DeltaTime;
+	}
+}
+
+void AHazardousWater::DamageActor()
+{
+	UGameplayStatics::ApplyDamage(UGameplayStatics::GetPlayerPawn(GetWorld(), 0), DamageComponent->GetDamage(), this->GetInstigatorController(), this,DamageComponent->GetDamageType());
+	Timer = TimeUntilDamage;
+}
+
+void AHazardousWater::SetMaxAcceleration(float Amount)
+{
+	if(Player)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("SET MAX ACCELERATION"));
+		Player->GetCharacterMovement()->MaxWalkSpeed = Amount;
+	}
+	
+}
+
+
+
---
+++ Source/SPM/HazardousWater.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HazardousWater.generated.h"
+
+class UDamageComponent;
+class UBoxComponent;
+
+UCLASS()
+class SPM_API AHazardousWater : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHazardousWater();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float TimeUntilDamage;
+
+	float Timer;
+	
+	UPROPERTY(EditAnywhere)
+	UDamageComponent* DamageComponent;
+	//The HazardousWater
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* Water;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UBoxComponent* WaterCollision;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsPlayerOnWater;
+
+	UPROPERTY()
+	ACharacter* Player;
+	
+	UFUNCTION(BlueprintCallable)
+	void DamageActor();
+
+	UFUNCTION(BlueprintCallable)
+	void SetMaxAcceleration(float Amount);
+
+
+
+
+
+};
