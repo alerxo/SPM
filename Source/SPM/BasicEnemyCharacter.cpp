f85096139f9edae630e6fb14180e2006250b23ed
Xandr03 <alexander.bakas@gmail.com>
2024-04-24 14:43:22 +0200

    Commit

--- Source/SPM/BasicEnemyCharacter.cpp
+++ Source/SPM/BasicEnemyCharacter.cpp
@@ -3,6 +3,23 @@
 //https://dev.epicgames.com/documentation/en-us/unreal-engine/behavior-tree-in-unreal-engine---quick-start-guide?application_version=5.0
 #include "BasicEnemyCharacter.h"
 
+/*Master Mind - Håller koll på spelare och spawnar fiender
+
+NEED TO WORK
+-Master mind AI - Controlls everything
+	-knows the player position and creates and estimate for the AI enemies to track 
+	-
+-Potentially SpawnerScript that the master mind takes controll over
+	- Spawn enemies at sertan point (Spawn points)
+	- Spawn enemies on event when cores shield is deactivated (While terminal timer ticks down)
+	- Spawn enemies distance from player behind(out of view)/the Master mind decide this based how the player plays
+	-
+-Each enemy cost a sertan amount of tokes 
+	- this is so the master mind decide what kinds of things to spawn
+	-
+*/
+
+
 // Sets default values
 ABasicEnemyCharacter::ABasicEnemyCharacter()
 {
